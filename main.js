dbaeaffa569d6f4cc6be1b37d3281a85f65428ed
Bao99257@donga.edu.vn <YourGitHubEmail>
2025-09-23 15:54:36 +0700

    .

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const panels = document.querySelectorAll(".panel");
-panels.forEach(item=>{
+panels.forEach(item => {
     console.log(item);
-    item.addEventListener("mouseenter",()=>{
+    item.addEventListener("mouseenter", () => {
         removeActive();
-        item.classList.add("active")
-    })
-    
-})
-function removeActive(){
-    panels.forEach((item)=>{
-        item.classList.remove("active")
-    })
+        item.classList.add("active");
+    });
+});
+
+function removeActive() {
+    panels.forEach((item) => {
+        item.classList.remove("active");
+    });
 }
